440b7e5d5ec30609a3119ffd05c70f62a6b8b073
gundamss <chauyuhin1998@gmail.com>
2022-05-12 22:42:42 -0600

    Two more steps

--- Program.cs
+++ Program.cs
@@ -2,11 +2,32 @@
 
 namespace cse210_02
 {
-    class Program
+    public class Program
     {
+
+        public static string continue_game = "y";
+        public static int playerscores = 300;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        Guessing guessinggame = new Guessing();
+        Rule gamerules = new Rule();
+        Console.WriteLine("Welcome to Hilo Game");
+        Console.WriteLine("Here are the following rules:");
+
+        do{
+
+
+        guessinggame.guessthecard();
+        gamerules.gameover();
+
+
+        Console.Write("Do you want to continue guessing? y/n ");
+        continue_game = Console.ReadLine();
+
+
+        } while (continue_game == "y");
+
+        Console.WriteLine("Thanks for playing!");
         }
     }
-}
+}   
